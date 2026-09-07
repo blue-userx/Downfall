9818948894e7d9e6788c02666ed7b97d430e3c67
lamali <lamali292@gmail.com>
2026-09-07 20:36:07 +0200

    feat(collector): overhaul collectible cards; implement new mechanics, rework card initialization, and update localization

--- CollectorCode/Cards/Collectibles/BygoneEffigyCard.cs
+++ CollectorCode/Cards/Collectibles/BygoneEffigyCard.cs
@@ -1,11 +1,19 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Collector.CollectorCode.Cards.Token;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models.Encounters;
 
 namespace Collector.CollectorCode.Cards.Collectibles;
 
-public class BygoneEffigyCard()
-    : Collectible<BygoneEffigyElite>(0, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy, 0.3f)
+public class BygoneEffigyCard : Collectible<BygoneEffigyElite>
 {
+    public BygoneEffigyCard() : base(0, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy, 0.3f)
+    {
+    }
+
+    protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
+    {
+        // Todo
+    }
 }
