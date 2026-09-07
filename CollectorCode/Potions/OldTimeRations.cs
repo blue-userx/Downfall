b09f1dd373eeb5a26b962323e6b814a42de1f5cd
SandTag <122702937+SandTag@users.noreply.github.com>
2026-09-07 19:33:57 +0100

    Swap pots
    Pots were backward

--- CollectorCode/Potions/OldTimeRations.cs
+++ CollectorCode/Potions/OldTimeRations.cs
@@ -1,10 +1,9 @@
 ﻿using BaseLib.Utils;
 using Collector.CollectorCode.Core;
-using MegaCrit.Sts2.Core.Commands;
+using Collector.CollectorCode.Extensions;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Potions;
-using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
 namespace Collector.CollectorCode.Potions;
@@ -12,20 +11,14 @@ namespace Collector.CollectorCode.Potions;
 [Pool(typeof(CollectorPotionPool))]
 public class OldTimeRations : CollectorPotionModel
 {
-    public OldTimeRations() : base(PotionRarity.Uncommon, PotionUsage.CombatOnly, TargetType.AnyEnemy)
+    public OldTimeRations() : base(PotionRarity.Uncommon, PotionUsage.CombatOnly, TargetType.AnyPlayer)
     {
-        WithVar("Increase", 2);
+        WithReserve(3);
     }
 
     protected override async Task OnUse(PlayerChoiceContext ctx, Creature? target)
     {
-        if  (target == null) return;
-        var debuffs = target.Powers
-            .Where(p => p.TypeForCurrentAmount == PowerType.Debuff)
-            .ToList();
-        foreach (var power in debuffs)
-        {
-            await PowerCmd.ModifyAmount(ctx, power, DynamicVars["Increase"].IntValue, power.Applier,null);
-        }
+        if (target?.Player == null) return;
+        await CollectorCmd.GetReserve(target.Player, DynamicVars.Reserve.IntValue);
     }
 }
