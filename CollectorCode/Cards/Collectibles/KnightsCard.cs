b79fc50b812e3243a44231d1caa62823b9ba261a
lamali <lamali292@gmail.com>
2026-09-09 17:17:04 +0200

    feat(collector): implement KnightsCardPower, update Knights Card mechanics, and improve related localization

--- CollectorCode/Cards/Collectibles/KnightsCard.cs
+++ CollectorCode/Cards/Collectibles/KnightsCard.cs
@@ -1,26 +1,29 @@
 ﻿using BaseLib.Utils;
 using Collector.CollectorCode.Cards.Token;
+using Collector.CollectorCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Afflictions;
 using MegaCrit.Sts2.Core.Models.Encounters;
+using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace Collector.CollectorCode.Cards.Collectibles;
 
 public class KnightsCard : Collectible<KnightsElite>
 {
-    public KnightsCard() : base(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self, 0.3f)
+    public KnightsCard() : base(2, CardType.Power, CardRarity.Uncommon, TargetType.Self, 0.3f)
     {
-        WithKeyword(CardKeyword.Ethereal);
-        WithCards(3, 1);
+        WithTip(CardKeyword.Ethereal);
+        WithPower<MachineLearningPower>(2, false);
+        WithPower<KnightsCardPower>(1, false);
+        WithKeyword(CardKeyword.Innate, UpgradeType.Add);
     }
 
     protected override async Task OnPlayInternal(PlayerChoiceContext ctx, CardPlay cardPlay)
     {
-        var cards = await CommonActions.Draw(this, ctx);
-        //foreach (var card in cards)
-        // Todo  
+        await CommonActions.ApplySelf<MachineLearningPower>(ctx, this);
+        await CommonActions.ApplySelf<KnightsCardPower>(ctx, this);
     }
 }
